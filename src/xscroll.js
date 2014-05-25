ca9f38d10b1b0e06842deb888d1ab9138445654e
ouxingzhi <ouxingzhi@vip.qq.com>
2014-05-25 22:45:41 +0800

    edit

--- src/xscroll.js
+++ src/xscroll.js
@@ -8,7 +8,8 @@ void function(window,document,undefined){
 	var ap = Array.prototype,
 		slice = ap.slice,
 		push = ap.push,
-		regSelector = /^#([a-z]\w+)|\.([a-z]\w+)|([a-z]+)/i
+		regSelector = /^#([a-z]\w+)|\.([a-z]\w+)|([a-z]+)/i,
+		regTag = /^[a-z]+$/i;
 	//简单选择器
 	function $(selector,rootNode){
 		rootNode = rootNode || document;
@@ -32,6 +33,50 @@ void function(window,document,undefined){
 		push.apply(rets,rootNode.querySelectorAll(selector));
 		return rets;
 	}
+	function isElement(o){
+		return !!(o && o.addEventListener && o.nodeType === 1);
+	}
+	function addEvent(el,type,fn){
+		el.addEventListener(type,fn,false);
+	}
+
+	function removeEvent(el,type,fn){
+		el.removeEventListener(type,fn);
+	}
+
+	function trigger(el,event){
+		el.dispatchEvent(event);
+	}
+
+	var C = function(div,attr,events){
+		return function(html){
+			var dom = null;
+			if(regTag.test(html)){
+				dom = document.createElement(html);
+			}else{
+				div.innerHTML = html;
+				var child = div.firstChild;
+				loop:do{
+					if(child && child.nodeType === 1) {
+						div.removeChild(child);
+						div.innerHTML = '';
+						dom = child;
+						break loop;
+					}
+				}while(child = child.nextSibling);
+			}
+			attr = attr || {};
+			for(var i in attr){
+				if(i === 'class') i = 'className';
+				dom[i] = attr[i];
+			}
+			events = events || {};
+			for(var i in events){
+				addEvent(dom,i,events[i]);
+			}
+			return dom;
+		};
+	}(document.createElement('div'));
 	//获得元素大小
 	function getSize(el,useBorder){
 		var width,height;
@@ -55,15 +100,141 @@ void function(window,document,undefined){
 		};
 	}
 	//取视口大小
-	function getViewSize(){
+	function getViewPortSize(){
 		return {
 			width:window.innerWidth,
 			height:window.innerHeight
 		}
 	}
 
+	var CLASS_PREFIX = 'xscroll-';
 
-	function xScroll(){
+	function xScroll(ops){
+		this.html;
+		//内层容器
+		this.innContainer;
+		//外层容器
+		this.outContainer;
+		//滚动条
+		this.scrollBar;
 
+		this._onTouchStart = this.onTouchStart.bind(this);
+		this._onTouchMove = this.onTouchMove.bind(this);
+		this._onTouchEnd = this.onTouchEnd.bind(this);
+		this.events = {};
+		this.setOption(ops);
+		this.init();
 	}
+
+	xScroll.prototype = {
+		constructor:xScroll,
+		//设置
+		on:function(type,data,fn,unique){
+			if(!(data instanceof Array)){
+				fn = data;
+				unique = fn;
+				data = null;
+			}
+			if(!(fn instanceof Function)) fn = function(){};
+			var events = this.events[type] = this.events[type] || [];
+			if(unique){
+				for(var i=events.length-1;i>-1;i--){
+					if(fn === events[i].fn) events.splice(i,1);
+				}
+			}
+			events.push({
+				data:data,
+				fn:fn
+			});
+			return this;
+		},
+		off:function(type,fn){
+			if(!type) return this;
+			var events;
+			if(fn){
+				var events = this.events[type];
+				if(!events) return this;
+				for(var i=events.length-1;i>-1;i--){
+					if(fn === events[i].fn) events.splice(i,1);
+				}
+			}else{
+				this.events[type] = null;
+			}
+			return this;
+		},
+		trigger:function(type,data){
+			var events = this.events[type],
+				self = this;
+			if(!events) return this;
+			events.forEach(function(o){
+				var args = [];
+				if(o.fn instanceof Function){
+					o.data && args.push(o.data);
+					data && args.push(data);
+					o.fn.apply(self,args);
+				}
+			});
+			return this;
+		},
+		setOption:function(ops){
+			if(ops.html){
+				if(typeof ops.html === 'string'){
+					this.html = C(ops.html);
+				}else if(isElement(ops.html)){
+					this.html = ops.html;
+				}
+			}
+			var viewSize = getViewPortSize();
+			if(ops.minWidth){
+				this.minWidth = ops.minWidth;
+			}else{
+				this.minWidth = viewSize.width;
+			}
+
+			if(ops.minHeight){
+				this.minHeight = ops.minHeight;
+			}else{
+				this.minHeight = viewSize.height;
+			}
+
+		},
+		init:function(){
+			this.createHTML();
+			this.buildEvent();
+		},
+		buildEvent:function(){
+			addEvent(this.outContainer,'touchstart',this._onTouchStart);
+			addEvent(this.outContainer,'touchmove',this._onTouchMove);
+			addEvent(this.outContainer,'touchend',this._onTouchEnd);
+		},
+		onTouchStart:function(){
+
+		},
+		onTouchMove:function(){
+
+		},
+		onTouchEnd:function(){
+
+		},
+		layout:function(){
+			var outSize = getSize(this.outContainer),
+				innSize = getSize(this.innContainer),
+				viewSize = getViewPortSize();
+
+		},
+		createHTML:function(){
+			this.outContainer = C('div',{
+				'class':CLASS_PREFIX+'outer-container'
+			});
+			this.innContainer = C('div',{
+				'class':CLASS_PREFIX+'inner-container'
+			});
+			this.scrollBar = C('div',{
+				'class':CLASS_PREFIX+'scrollbar'
+			});
+			this.outContainer.appendChild(this.innContainer);
+			this.outContainer.appendChild(this.scrollBar);
+			this.innContainer.appendChild(this.html);
+		}
+	};
 }(window,document);
